d88a9f034ada258df0efdfcbc6cdaebf80e6c1d6
RobelAlemayehu <robelalemayehu838@gmail.com>
2025-12-22 17:33:01 +0300

    feat: add an environment variable

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,10 +1,13 @@
-const { createPool } = require('mysql')
+require('dotenv').config()
+
+
+const mysql = require('mysql2')
 
 const pool  = createPool({
-    host:"localhost",
-    user:"root",
-    password:"",
-    database:"Your_tera",
+    host:process.env.DB_HOST,
+    user:process.env.DB_USER,
+    password:process.env.DB_PASSWORD,
+    database:process.env.DB_NAME,
     connectionLimit:4
 })
 
